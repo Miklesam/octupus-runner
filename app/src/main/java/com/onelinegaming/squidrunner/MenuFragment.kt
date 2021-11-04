4a244c118668d02fc4c48b12d50619ccf67ba3b5
Samarkin Mikhail <mikle.samarkin@gmail.com>
2021-11-04 21:27:00 +0300

    first screen permitions

--- app/src/main/java/com/onelinegaming/squidrunner/MenuFragment.kt
+++ app/src/main/java/com/onelinegaming/squidrunner/MenuFragment.kt
@@ -1,7 +1,10 @@
 package com.onelinegaming.squidrunner
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_menu.*
@@ -13,5 +16,20 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
         game_1_bttn.setOnClickListener {
             findNavController().navigate(R.id.action_menuFragment_to_gameOneFragment)
         }
+        if (allPermissionsGranted()) {
+
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                StartActivity.REQUIRED_PERMISSIONS,
+                StartActivity.REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun allPermissionsGranted() = StartActivity.REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
